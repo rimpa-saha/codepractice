12c31886f4c9c4222328922d3d1bd6feb2c9a568
rimpa-saha <rimpasaha0123@gmail.com>
2024-12-15 14:51:24 +0530

    easy

---
+++ c_practice/day1_14/Q2.c
@@ -0,0 +1,10 @@
+//1. Write a program in C to display the first 10 natural numbers. Computer programming
+//Expected Output :
+//1 2 3 4 5 6 7 8 9 10
+#include<stdio.h>
+int main(){
+for(int i=1;i<=10;i++){
+    printf("%d ",i);
+}
+    return 0;
+}
