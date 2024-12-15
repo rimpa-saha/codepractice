fae0444ff8c3f2ec029cc0811bfaf1ba852bd5ce
rimpa-saha <rimpasaha0123@gmail.com>
2024-12-15 15:10:53 +0530

    easy

---
+++ c_practice/day1_14/Q3.c
@@ -0,0 +1,16 @@
+//2. Write a C program to compute the sum of the first 10 natural numbers.
+//Expected Output :
+//The first 10 natural number is :
+//1 2 3 4 5 6 7 8 9 10
+//The Sum is : 55
+#include<stdio.h>
+int main(){
+int sum=0;
+ printf("the first 10 natural number is:\n");
+ for(int i=1;i<=10;i++){
+    printf("%d ",i);
+    sum=sum+i;
+ }
+ printf("\nthe sum is: %d ",sum);
+    return 0;
+}
