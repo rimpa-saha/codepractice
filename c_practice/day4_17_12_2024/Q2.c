eb5ee5f27113096218df65220fe612428a8a9160
rimpa-saha <rimpasaha0123@gmail.com>
2024-12-17 21:04:17 +0530

    hard

---
+++ c_practice/day4_17_12_2024/Q2.c
@@ -0,0 +1,29 @@
+/*Write a program in C to display a pattern like a right angle triangle using an asterisk.
+
+Thepattern like :
+   1
+  123
+ 12345
+1234567            */
+
+#include<stdio.h>
+int main(){
+int n;
+printf("enter the number of rows:");
+scanf("%d",&n);
+int nst=1;
+int nsp=n-1;
+for(int i=1;i<=n;i++){
+    for(int k=1;k<=nsp;k++){
+        printf(" ");
+    }
+    nsp=nsp-1;
+    for(int j=1;j<=nst;j++){
+        printf("%d",j);
+    }
+    nst=nst+2;
+    printf("\n");
+}
+
+    return 0;
+}
