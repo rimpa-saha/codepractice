906c1ec0fb66bf7a0e11e3dfbe60a474ce77c190
rimpa-saha <rimpasaha0123@gmail.com>
2024-12-18 15:42:43 +0530

    easy

--- c_practice/day5_18_12_2024/Q1.c
+++ c_practice/day5_18_12_2024/Q1.c
@@ -10,6 +10,6 @@ scanf("%d",&m);
 printf("enter the number2:");
 scanf("%d",&n);
 if(m==n)printf("number1 and number2 are equal");
-else("number1 and number2 are not equal");
+else printf("number1 and number2 are not equal");
     return 0;
 }
