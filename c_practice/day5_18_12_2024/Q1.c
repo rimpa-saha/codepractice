bd7cd544b6a44e787583aafff87f006c31286017
rimpa-saha <rimpasaha0123@gmail.com>
2024-12-18 15:33:20 +0530

    easy

---
+++ c_practice/day5_18_12_2024/Q1.c
@@ -0,0 +1,15 @@
+/*1. Write a C program to accept two integers and check whether they are equal or not.
+Test Data : 15 15
+Expected Output :
+Number1 and Number2 are equal*/
+#include<stdio.h>
+int main(){
+int m,n;
+printf("enter the number1:");
+scanf("%d",&m);
+printf("enter the number2:");
+scanf("%d",&n);
+if(m==n)printf("number1 and number2 are equal");
+else("number1 and number2 are not equal");
+    return 0;
+}
