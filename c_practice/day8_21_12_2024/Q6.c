94bcf09500e08ee77457986760aec62ae7c13558
rimpa-saha <rimpasaha0123@gmail.com>
2024-12-21 23:13:28 +0530

    hard

---
+++ c_practice/day8_21_12_2024/Q6.c
@@ -0,0 +1,31 @@
+/*Write a program in C to accept a grade and display the equivalent description:
+Grade	Description
+E	Excellent
+V	Very Good
+G	Good
+A	Average
+F	Fail*/
+#include<stdio.h>
+int main(){
+ char grade;
+ printf("input the grade(E,V,G,A,F)");
+ scanf("%s",&grade);
+ switch (grade) {
+        case 'E':
+            printf("Description: Excellent\n");
+            break;\
+        case 'V':
+        printf("description:very good\n");
+        break;
+        case 'G':
+        printf("description: good\n");
+        break;
+        case 'A':
+        printf("description:Average\n");
+        break;
+        case 'F':
+        printf("description:fail\n");
+        break;
+ }
+          return 0;
+}
