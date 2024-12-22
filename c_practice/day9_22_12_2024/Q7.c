6534cf2ff3809d3c8804be1445e9c5464d49ae67
rimpa-saha <rimpasaha0123@gmail.com>
2024-12-22 12:06:01 +0530

    medium

---
+++ c_practice/day9_22_12_2024/Q7.c
@@ -0,0 +1,14 @@
+/*Write a C program that calculates the product of numbers from 1 to 5 using a while loop.*/
+#include <stdio.h>
+
+int main() {
+    int number = 1; 
+    int product = 1;
+    while (number <= 5) {
+        product *= number;
+        printf("Current number: %d, Current product: %d\n", number, product);
+        number++;
+    }
+
+    return 0; 
+}
