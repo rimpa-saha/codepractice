acfcdfbfcca1f98639283127cf23f25850b6d503
rimpa-saha <rimpasaha0123@gmail.com>
2025-05-03 11:52:27 +0530

    easy

---
+++ c_practice/day19_03_05_2025/Q5.c
@@ -0,0 +1,11 @@
+//square root of any number
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a;
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    float root=sqrt(a);
+    printf("the srure root is %f",root);
+    return 0;
+}
