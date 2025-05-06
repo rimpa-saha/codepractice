85cd3ca9aadf03742b81fa7e9407ec80919b1ac7
rimpa-saha <rimpasaha0123@gmail.com>
2025-05-06 08:09:56 +0530

    easy

---
+++ c_practice/day21_05_05_2025/Q1.c
@@ -0,0 +1,17 @@
+//write two functions-one to print"hello" and second to print "goodbye"
+
+#include<stdio.h>
+void printhello();
+void printgoodbye();
+
+int main(){
+    printhello();
+    printgoodbye();
+return 0;
+}
+void printhello(){
+    printf("hello\n");
+}
+void printgoodbye(){
+    printf("goodbye\n");
+}
